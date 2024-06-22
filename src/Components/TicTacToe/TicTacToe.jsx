1e75cf5ca879ebc066cd6fcbb211cfd70d9c0b75
harshit <harshit.jagarlamudi@outlook.com>
2024-06-22 19:55:01 +0100

    setup the board

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -9,9 +9,27 @@ const TicTacToe = () => {
         <div>
             <div className='container'>
                 <h1 className="title">Tic Tac Toe<span></span></h1>
-                <div className="board"></div>
+                <div className="board">
+                    <div className="row1">
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                    </div>
+                    <div className="row1">
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                    </div>
+                    <div className="row1">
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                        <div className="boxes"></div>
+                    </div>
+
+                </div>
+                <button className="reset">Reset</button>
             </div>
-            <button className="reset">Reset</button>
+           
 
         </div>
     )
